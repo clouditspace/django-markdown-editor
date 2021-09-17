45772bf70db721a6284aa1960aadb4786f93e61d
root <root@sonarcube.us-central1-a.c.exchangeonline-979.internal>
2021-09-18 00:52:29 +0530

    add unit test and static files

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -94,13 +94,17 @@ WSGI_APPLICATION = 'martor_demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'manutest',
+       'USERNAME': 'manuphilip',
+       'PASSWORD': 'teddybear',
+       'HOST': '52.168.49.29',
+       'PORT': 5432,
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
